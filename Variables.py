36082f1656dcc9044740eb3fee9b9da1bd55adfb
ArturCabman <katokataka@gmail.com>
2024-11-09 19:36:56 +0700

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+Number_of_HA = 12
+Number_of_SA = 1.5
+Course_name = 'Pyton'
+time_for_one_task = Number_of_SA * 60 / Number_of_HA
+print('Курс:' + str(Course_name), 'Всего задач:' + str(Number_of_HA), 'среднее время выполнения ' + str(time_for_one_task), 'минут')
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Artur'
+print ('Name: ' + name)
+age = 60
+print('Age: ' + str(age))
+print('New age: ' + str(age + 1))
+is_Student = 1
+name2 = (1 == 1)
+print('is_Student: ' + str(name2))
